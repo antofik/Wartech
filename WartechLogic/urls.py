9c08f5ede9a52730a27c05b560e3696098dbf6dd
Anton <antofik@gmail.com>
2013-08-28 17:27:08 +0400

    url change

--- WartechLogic/urls.py
+++ WartechLogic/urls.py
@@ -9,17 +9,17 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', main.views.home),
     
-    url(r'get_all_users^$', main.views.get_all_users),
-    url(r'request_fight^$', main.views.request_fight),
-    url(r'get_all_modules^$', main.views.get_all_modules),
-    url(r'get_user_robot^$', main.views.get_user_robot),
-    url(r'get_user_modules^$', main.views.get_user_modules),
-    url(r'set_module_to_slot^$', main.views.set_module_to_slot),
+    url(r'^get_all_users$', main.views.get_all_users),
+    url(r'^request_fight$', main.views.request_fight),
+    url(r'^get_all_modules$', main.views.get_all_modules),
+    url(r'^get_user_robot$', main.views.get_user_robot),
+    url(r'^get_user_modules$', main.views.get_user_modules),
+    url(r'^set_module_to_slot$', main.views.set_module_to_slot),
     
-    url(r'create_new_user^$', main.views.create_new_user),
-    url(r'login^$', main.views.login),
+    url(r'^create_new_user$', main.views.create_new_user),
+    url(r'^login$', main.views.login),
     
-    url(r'dummy^$', main.views.dummy),
+    url(r'^dummy$', main.views.dummy),
     
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
