0769c1ab7691a5d349bd68c2faae1f78dc7cfde8
Anton <antofik@gmail.com>
2013-08-29 15:36:15 +0400

    CORS for POST

--- WartechLogic/main/views.py
+++ WartechLogic/main/views.py
@@ -12,7 +12,7 @@ def JsonResponse(data):
     response["Access-Control-Allow-Origin"] = "*"  
     response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"  
     response["Access-Control-Max-Age"] = "1000"  
-    response["Access-Control-Allow-Headers"] = "*"
+    response["Access-Control-Allow-Headers"] = "Origin, Content-Type"
     return response
 
 
