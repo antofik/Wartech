5f581b31fae1e02b92b071a9df0c763603843529
Anton <antofik@gmail.com>
2013-08-29 16:33:43 +0400

    test

--- WartechLogic/main/views.py
+++ WartechLogic/main/views.py
@@ -16,6 +16,7 @@ def JsonResponse(request, data):
     response["Access-Control-Allow-Origin"] = "*"  
     response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"  
     response["Access-Control-Max-Age"] = "86400"  
+    response["x-test"] = "super secret header"
     if request.method == "OPTIONS":
         response["Access-Control-Allow-Headers"] = "origin, content-type, x-requested-with, accept, authorization"
     return response
