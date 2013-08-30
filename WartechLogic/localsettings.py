1b3116b73ffe28f8c4d6de66610cedb036fd031e
Anton Naumenko <antofik@gmail.com>
2013-08-30 21:54:56 +0400

    db => mysql

--- WartechLogic/localsettings.py
+++ WartechLogic/localsettings.py
@@ -7,14 +7,14 @@ PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, "sqldb.lite"),                      # Or path to database file if using sqlite3.
-        #'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'NAME': 'wartech.logic',                      # Or path to database file if using sqlite3.
-        'USER': 'wartech.logic',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'wartech.logic',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': os.path.join(PROJECT_PATH, "sqldb.lite"),                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wartech.logic',
+        'USER': 'wartech.logic',
+        'PASSWORD': 'fda34k2;dSA#%%435rfda2S',
+        'HOST': 'wartech.logic',
+        'PORT': '',
         #'OPTIONS': {'read_default_file': os.path.join(PROJECT_PATH, 'mysql.cnf'),},
     }
 }
