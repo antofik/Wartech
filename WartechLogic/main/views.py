866f0166d7741c3d09454b93a74d639d592162db
Anton Naumenko <antofik@gmail.com>
2013-08-31 14:15:38 +0400

    test

--- WartechLogic/main/views.py
+++ WartechLogic/main/views.py
@@ -24,7 +24,10 @@ def JsonResponse(request, data):
 
 
 def home(request):
-    text = open(r'/github/Wartech/.git/ORIG_HEAD').read()
+    try:
+        text = open(r'/github/Wartech/.git/ORIG_HEAD').read()
+    except Exception, e:
+        text = str(e)
     return render_to_response('home.html', {'git_version': text}, context_instance=RequestContext(request))
 
 
