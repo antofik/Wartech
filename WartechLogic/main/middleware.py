25711d12695f22313041d709070b71c0fd6d0e3a
Anton <antofik@gmail.com>
2013-09-03 10:35:00 +0400

    get/post sessionid will dominate over cookie sessionid

--- WartechLogic/main/middleware.py
+++ WartechLogic/main/middleware.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class CooklessMiddleware(object):
     def process_request(self, request):
-        if settings.SESSION_COOKIE_NAME not in request.COOKIES:
+        #if settings.SESSION_COOKIE_NAME not in request.COOKIES:
             if request.method == "GET" and settings.SESSION_COOKIE_NAME in request.GET:
                 request.COOKIES[settings.SESSION_COOKIE_NAME] = request.GET[settings.SESSION_COOKIE_NAME]
             elif request.method == "POST" and settings.SESSION_COOKIE_NAME in request.POST:
