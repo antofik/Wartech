f008ac5f083d32223f05257f869d4f3a762804a3
Artem Naumenko <entsupml@gmail.com>
2013-09-04 21:28:00 +0400

    proxy.php fix

--- www/oauth.php
+++ www/oauth.php
@@ -3,4 +3,4 @@ $adapter = $_GET['adapter'];
 $domain = $_GET['domain'];
 $code = $_GET['code'];
 
-header("location: /configure.html?oauth=vk.com&code=$code");
+header("location: //$domain/configure.html?oauth=$adapter&code=$code");
