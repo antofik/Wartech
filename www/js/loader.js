3a09cd2e56fc9cce663b91a480594779d97dd10d
Artem Naumenko <entsupml@gmail.com>
2013-09-07 21:17:06 +0400

    next

--- www/js/loader.js
+++ www/js/loader.js
@@ -1,18 +1,15 @@
 var cache = [];
 loader = {
+    load: function(scriptName, callback){
+        if (cache[scriptName] != undefined)
+            return callback();
 
-}
-loader.load = function(scriptName, callback){
-    if (cache[scriptName] != undefined)
-        return callback();
-
-    $.ajax({
-        url: 'js/' + scriptName + '.js',
-        dataType: 'html',
-        success: function(reply){
-            eval(reply);
+        var script = document.createElement('script');
+        script.onload = function(){
             cache[url] = true;
             callback();
         }
-    });
+        script.url = 'js/' + scriptName + '.js';
+        $('body').appendChild(script);
+    }
 }
