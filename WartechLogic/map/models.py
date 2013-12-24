c4c4f70b66fba60acf0304db074322639723b89c
Anton <antofik@gmail.com>
2013-12-24 18:01:44 +0400

    map

--- WartechLogic/map/models.py
+++ WartechLogic/map/models.py
@@ -1,4 +1,104 @@
-import json
 from django.db.models import *
+import json
 
+
+class MapTile(Model):
+    id = AutoField( primary_key=True)
+    ready_level = PositiveSmallIntegerField()
+    type = PositiveSmallIntegerField()
+    point = BigIntegerField()
+    data = TextField()
+
+
+class ChemicalElements(object):
+    Ar = 0
+    Fe = 1
+    Lu = 2
+    Rz = 3
+    Es = 4
+    Uq = 5
+    Io = 7
+    Bi = 8
+    Ya = 9
+    Ze = 10
+    Xj = 11
+
+
+class Material(object):
+    Table = {}
+
+    def __init__(self, name, state, substate, *elements):
+        self.State = state
+        self.Substate = substate
+        self.Elements = elements
+        self.Name = name
+        self.ChemicalName = self.get_name()
+        Material.Table[self.Name] = self
+
+    def get_name(self):
+        name = ''.join(['%s%s' for (el, n) in self.Elements])
+        return '%s-%s-%s' % (name, self.State, self.Substate)
+
+    def __str__(self):
+        return self.Name
+
+    @classmethod
+    def register(cls, name, Material):
+        print 'registering', name
+        setattr(cls, name, Material)
+
+    @staticmethod
+    def get(name, chemicalName=None):
+        if name in Material.Table:
+            return Material.Table[name]
+        #todo
+        #state, substate, elements = Material.parse_chemical_name(chemicalName)
+        #material = Material(name, )
+        return Material.Table[Materials.Water]
+
+    @staticmethod
+    def parse_chemical_name(name):
+        #todo
+        return None, None, None
+
+
+class MaterialState(object):
+    Liquid = 0
+    Dry = 1
+    Solid = 2
+
+
+class MaterialSubstate(object):
+    Soft = 0
+    Hard = 1
+
+
+class Materials(object):
+    Water = "Water"
+    Rock = "Rock"
+    Sand = "Sand"
+    Soil = "Soil"
+
+_ = ChemicalElements
+Material.register(Materials.Water, Material(MaterialState.Liquid, MaterialSubstate.Soft, (_.Ar, 2), (_.Lu, 1)))
+Material.register(Materials.Rock, Material(MaterialState.Solid, MaterialSubstate.Hard, (_.Fe, 5), (_.Lu, 1), (_.Es, 2)))
+Material.register(Materials.Sand, Material(MaterialState.Dry, MaterialSubstate.Soft, (_.Ar, 1), (_.Es, 1), (_.Rz, 2)))
+Material.register(Materials.Soil, Material(MaterialState.Dry, MaterialSubstate.Hard, (_.Ar, 1), (_.Lu, 2), (_.Fe, 2), (_.Rz, 1), (_.Es, 1)))
+
+
+class MapCell(object):
+    def __init__(self, x, y, height, material_name):
+        self.X = x
+        self.Y = y
+        self.Height = height
+        self.Material = Material.get(material_name)
+
+    @staticmethod
+    def parse(x, y, json_value):
+        values = json.loads(json_value)
+        cell = MapCell(x,y, values['height'], values['material_name'])
+        return cell
+
+    def json(self):
+        return '{height:%s,material_name:%s}' % (self.X, self.Y, self.Height, self.Material.Name)
 
--- WartechLogic/map/views.py
+++ WartechLogic/map/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 import json
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.db.models import Q
 from models import *
 from PIL import Image
 
@@ -21,6 +22,64 @@ def JsonResponse(request, data):
     if request.method == "OPTIONS":
         response["Access-Control-Allow-Headers"] = "origin, content-type, x-requested-with, accept, authorization"
     return response
+
+
+def generate_map(request, sx, sy):
+    """
+    sx,sy=0..999
+    """
+    print sx, sy
+    sx, sy = int(sx), int(sy)
+    im = Image.open("media/images/map.png")
+    pixels = im.load()
+    pixel = pixels[sx, sy]
+
+    material = Materials.Water
+    height = 0
+    if pixel == 75:
+        material = Materials.Water
+        height = 0
+    elif pixel == 2:
+        material = Materials.Soil
+        height = 2
+    elif pixel == 3:
+        material = Materials.Soil
+        height = 4
+    elif pixel == 5:
+        material = Materials.Soil
+        height = 6
+    elif pixel == 6:
+        material = Materials.Soil
+        height = 8
+
+    map = []
+    for x in xrange(1000):
+        line = []
+        map.append(line)
+        for y in xrange(1000):
+            line.append(MapCell(sx + x, sy + y, height, material))
+
+    for x in xrange(10):
+        for y in xrange(10):
+            data = [[i.json() for i in line[x*100:x*100+100]] for line in map[y*100:y*100+100]]
+            print data
+            point = (sx + x) * 1000000 + (sy + y)
+            m = MapTile(point=point, type=4, data=data)
+            m.save()
+
+
+def get_map(sx, sy):
+    """
+    x=0..9999, y=0.9999
+    result contain 100x100 cells
+    """
+    point = sx * 1000000 + sy
+    m = MapTile.objects.get(Q(point=point), Q(type=4))
+    if not m:
+        generate_map(sx//10, sy//10)
+        m = MapTile.objects.get(Q(point=point), Q(type=4))
+    return m.data
+
 
 def get(request, sx, sy):
     sx = int(sx)
